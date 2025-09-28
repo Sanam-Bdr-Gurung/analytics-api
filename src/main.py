41c82e039c5147e5c1f64bb56f383103160e0285
Sanam Gurung <sanam.bdr.gurung@gmail.com>
2025-09-28 19:12:41 +0545

    created, organized and injected the routing on fastapi router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+from sys import prefix
 from typing import Union
 
 from fastapi import FastAPI
+from api.events import router as events_router
 
 app = FastAPI()
+app.include_router(events_router,prefix= "/api/events")
 
 
 @app.get("/")
